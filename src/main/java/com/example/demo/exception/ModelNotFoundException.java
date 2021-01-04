ce6ba23ee7a110350e2167cc2be51801a4d5372a
clozarr <clozanoarroyo@gmail.com>
2021-01-04 00:45:07 -0500

    Configuración inicial del proyecto

---
+++ src/main/java/com/example/demo/exception/ModelNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.demo.exception;
+
+public class ModelNotFoundException extends RuntimeException {
+
+    public ModelNotFoundException(String message){
+        super(message);
+    }
+}
